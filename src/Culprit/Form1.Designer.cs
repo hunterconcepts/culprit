ad06b0eec8904397430a17541a02a99968c8693a
hunterconcepts <hunter@hunterconcepts.com>
2023-03-31 09:34:50 -0400

    initial

---
+++ src/Culprit/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Culprit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderTreeView = new System.Windows.Forms.TreeView();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderTextBox = new System.Windows.Forms.TextBox();
+            this.scanButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // folderTreeView
+            // 
+            this.folderTreeView.Location = new System.Drawing.Point(12, 76);
+            this.folderTreeView.Name = "folderTreeView";
+            this.folderTreeView.Size = new System.Drawing.Size(803, 651);
+            this.folderTreeView.TabIndex = 0;
+            // 
+            // folderTextBox
+            // 
+            this.folderTextBox.Location = new System.Drawing.Point(12, 50);
+            this.folderTextBox.Name = "folderTextBox";
+            this.folderTextBox.Size = new System.Drawing.Size(345, 20);
+            this.folderTextBox.TabIndex = 1;
+            // 
+            // scanButton
+            // 
+            this.scanButton.Location = new System.Drawing.Point(364, 50);
+            this.scanButton.Name = "scanButton";
+            this.scanButton.Size = new System.Drawing.Size(75, 23);
+            this.scanButton.TabIndex = 2;
+            this.scanButton.Text = "Scan";
+            this.scanButton.UseVisualStyleBackColor = true;
+            this.scanButton.Click += new System.EventHandler(this.scanButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1334, 826);
+            this.Controls.Add(this.scanButton);
+            this.Controls.Add(this.folderTextBox);
+            this.Controls.Add(this.folderTreeView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView folderTreeView;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox folderTextBox;
+        private System.Windows.Forms.Button scanButton;
+    }
+}
+
---
+++ src/Culprit/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Culprit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void scanButton_Click(object sender, EventArgs e)
+        {
+            folderTreeView.Nodes.Clear();
+
+            var folder = folderTextBox.Text;
+            ScanDirectory(folder);
+
+        }
+
+        private void ScanDirectory(string folder, TreeNode parent = null)
+        {
+            var target = new DirectoryInfo(folder);
+
+            foreach (var f in target.GetDirectories())
+            {
+                try
+                {
+                    var size = DirectorySize(f);
+                    var fileSize = GetFileSize(size);
+
+                    var node = new TreeNode(f.Name + " (" + GetFileSize(size) + ")");
+
+                    if (fileSize.Contains("GB"))
+                    {
+                        node.BackColor = Color.Red;
+                    }
+
+                    if (parent == null)
+                    {
+                        folderTreeView.Nodes.Add(node);
+                    }
+                    else
+                    {
+                        parent.Nodes.Add(node);
+                    }
+
+                    if (f.GetDirectories().Any())
+                    {
+                        ScanDirectory(f.FullName, node);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    folderTreeView.Nodes.Add(f.Name + " (" + ex.Message + ")");
+                }
+            }
+        }
+
+        public string GetFileSize(double len)
+        {
+            string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+            
+            int order = 0;
+            while (len >= 1024 && order < sizes.Length - 1)
+            {
+                order++;
+                len = len / 1024;
+            }
+
+            // Adjust the format string to your preferences. For example "{0:0.#}{1}" would
+            // show a single decimal place, and no space.
+            return string.Format("{0:0.##} {1}", len, sizes[order]);
+        }
+
+        public static long DirectorySize(DirectoryInfo directory)
+        {
+            long size = 0;
+            // Add file sizes.
+            FileInfo[] fis = directory.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+            // Add subdirectory sizes.
+            DirectoryInfo[] dis = directory.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += DirectorySize(di);
+            }
+            return size;
+        }
+    }
+}
